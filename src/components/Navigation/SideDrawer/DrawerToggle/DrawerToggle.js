5f3ea89ea88a474a07a7e2c32605470c20c87d5c
Zak <zakary.howard94@gmail.com>
2019-02-14 13:46:11 +0100

    created sidedrawer css file and drawertoggle function correctly accesses previous state of sidedrawer rather than just setting the boolean to whatever it is not

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './DrawerToggle.module.css'
+
+const drawerToggle = (props) => (
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        MENU
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
+
+export default drawerToggle;
