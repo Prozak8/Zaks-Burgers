66ea5a76835b254a07a8205c5102f6acd536dc60
Zak <zakary.howard94@gmail.com>
2019-02-25 14:29:32 +0100

    onChange listener added to all input values with function listening to deep nesting of value in state. Clones the current state and morphs it on change.

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,23 +10,27 @@ const input = (props) => {
             inputElement = <input 
                 className={classes.InputElement}
                 {...props.elementConfig} 
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed}/>;
             break;
         case('textarea'):
             inputElement = <textarea 
                 className={classes.InputElement}
                 {...props.elementConfig} 
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed}/>;
             break;
         case('select'):
             inputElement = (
                 <select 
                     className={classes.InputElement}
-                    {...props.elementConfig} 
-                    value={props.value}>
+                    value={props.value}
+                    onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
-                        <option key={option.value} value={option.value}>
-                            {option.displayValue}
+                        <option 
+                            key={option.value} 
+                            value={option.value}>
+                                {option.displayValue}
                         </option>
                     ))}
                 </select> 
@@ -36,7 +40,8 @@ const input = (props) => {
             inputElement = <input 
                 className={classes.InputElement}
                 {...props.elementConfig} 
-                value={props.value}/>;
+                value={props.value}
+                onChange={props.changed}/>;
     }
 
     return (
