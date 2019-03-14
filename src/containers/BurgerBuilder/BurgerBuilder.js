f96e70265d91b4636270e911cdd3af4ace22dc83
Zak <zakary.howard94@gmail.com>
2019-03-14 13:38:19 +0100

    rootReducer now takes in orderReducer through combineReducers, tried to initialize purchasing state within ContactDetails component but was causing error on redirect. Now added to purchaseContinueHandler, after checkout user redirected to homepage, orderData passed into checkout component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,6 +41,7 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
+    this.props.onInitPurchase()
     this.props.history.push("/checkout")
   }
 
@@ -97,9 +98,9 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice,
-    error: state.error,
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    error: state.burgerBuilder.error,
   }
 }
 
@@ -108,6 +109,7 @@ const mapDispatchToProps = dispatch => {
     onIngredientAdded: ingName => dispatch(actions.addIngredient(ingName)),
     onIngredientRemoved: ingName => dispatch(actions.removeIngredient(ingName)),
     onInitIngredients: () => dispatch(actions.initIngredients()),
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
   }
 }
 
