218d9c76a43d0010e1655aa0b5ed1a96cfc29eb7
Zak <zakary.howard94@gmail.com>
2019-03-20 15:24:32 +0100

    added route for Auth component to APP. New Auth Container created. Auth Page added to navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react"
+import { Route, Switch } from "react-router-dom"
 
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
+import Layout from "./hoc/Layout/Layout"
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
+import Checkout from "./containers/Checkout/Checkout"
+import Orders from "./containers/Orders/Orders"
+import Auth from "./containers/Auth/Auth"
 
 class App extends Component {
   render() {
@@ -14,12 +15,13 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react"
 
-import classes from './NavigationItems.module.css';
-import NavigationItem from './NavigationItem/NavigationItem';
+import classes from "./NavigationItems.module.css"
+import NavigationItem from "./NavigationItem/NavigationItem"
 
 const navigationItems = () => (
-    <ul className={classes.NavigationItems}>
-        <NavigationItem link="/">Burger Builder</NavigationItem>
-        <NavigationItem link="/orders">Orders</NavigationItem>
-    </ul>
-);
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/">Burger Builder</NavigationItem>
+    <NavigationItem link="/orders">Orders</NavigationItem>
+    <NavigationItem link="/auth">Authenticate</NavigationItem>
+  </ul>
+)
 
-export default navigationItems;
+export default navigationItems
