69785f1e83b9498d6e6138b4ea114604d07cd2d6
Zak <zakary.howard94@gmail.com>
2019-03-25 10:32:34 +0100

    persisting login token stored in localstorage. Fixed bug that logged user out on refresh

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -44,6 +44,12 @@ class Auth extends Component {
     isSignup: true,
   }
 
+  componentDidMount() {
+    if (!this.props.buildingBurger && this.props.authRedirectPath !== "/") {
+      this.props.onSetAuthRedirectPath()
+    }
+  }
+
   checkValidity(value, rules) {
     let isValid = true
     if (!rules) {
@@ -102,11 +108,6 @@ class Auth extends Component {
       })
     }
 
-    let authRedirect = null
-    if (this.props.isAuthenticated) {
-      authRedirect = <Redirect to="/" />
-    }
-
     let form = formElementsArray.map(formElement => (
       <Input
         key={formElement.id}
@@ -130,6 +131,11 @@ class Auth extends Component {
       errorMessage = <p>{this.props.error.message}</p>
     }
 
+    let authRedirect = null
+    if (this.props.isAuthenticated) {
+      authRedirect = <Redirect to={this.props.authRedirectPath} />
+    }
+
     return (
       <div className={classes.Auth}>
         {authRedirect}
@@ -151,6 +157,8 @@ const mapStateToProps = state => {
     loading: state.auth.loading,
     error: state.auth.error,
     isAuthenticated: state.auth.token != null,
+    buildingBurger: state.burgerBuilder.building,
+    authRedirectPath: state.auth.authRedirectPath,
   }
 }
 
@@ -158,6 +166,7 @@ const mapDispatchToProps = dispatch => {
   return {
     onAuth: (email, password, isSignup) =>
       dispatch(actions.auth(email, password, isSignup)),
+    onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath("/")),
   }
 }
 
