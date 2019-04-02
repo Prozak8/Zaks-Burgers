48ecf021702382d20aa2cfd299abca771fa4d05b
Zak <zakary.howard94@gmail.com>
2019-04-02 11:11:48 +0200

    indentation, clicking a link in the sidedrawer hides the modal on click

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -25,7 +25,6 @@ export const setIngredients = ingredients => {
 export const fetchIngredientsFailed = () => {
   return {
     type: actionTypes.FETCH_INGREDIENTS_FAILED,
-    error: error,
   }
 }
 
