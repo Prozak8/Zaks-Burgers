bcadccd6a9e6a7a892757a535d3c94ae25071bae
Zak <zakary.howard94@gmail.com>
2019-04-02 12:25:02 +0200

    removed console.logs

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,29 +1,28 @@
-import React from 'react';
+import React from "react"
 
-import classes from './Burger.module.css';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from "./Burger.module.css"
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 
-const burger = ( props ) => {
-    let transformedIngredients = Object.keys( props.ingredients )
-    .map( ingKey => {
-        return [...Array( props.ingredients[ ingKey ])].map( (_, i) => {
-            return <BurgerIngredient key={ingKey + i} type={ingKey} />
-        } );
-    } )
-    .reduce( ( arr, el ) => {
-        return arr.concat( el )
-    }, []);
-    if ( transformedIngredients.length === 0 ) {
-        transformedIngredients = <p>Please add ingredients.</p>
-    }
-    console.log(transformedIngredients)
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top" />
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    );
+const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, i) => {
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />
+      })
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, [])
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add ingredients.</p>
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  )
 }
 
-export default burger;
+export default burger
