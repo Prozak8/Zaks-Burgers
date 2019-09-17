5cb9c2c3971fc93137a36aba2307fde408ff6548
Zak <zakary.howard94@gmail.com>
2019-09-17 15:40:57 +0200

    Updates

---
+++ precache-manifest.9019aa68eff1ed04cab156e44e2392c3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5e4d8f740657612282c",
+    "url": "/Prozak8/Zaks-Burgers/static/css/main.6b2bdfd9.chunk.css"
+  },
+  {
+    "revision": "d5e4d8f740657612282c",
+    "url": "/Prozak8/Zaks-Burgers/static/js/main.d5e4d8f7.chunk.js"
+  },
+  {
+    "revision": "b205313c5f3ecb729bbe",
+    "url": "/Prozak8/Zaks-Burgers/static/js/runtime~main.b205313c.js"
+  },
+  {
+    "revision": "1c42ade88da96e5a338c",
+    "url": "/Prozak8/Zaks-Burgers/static/js/2.1c42ade8.chunk.js"
+  },
+  {
+    "revision": "ef6d552244850b3768a8",
+    "url": "/Prozak8/Zaks-Burgers/static/css/3.4a013529.chunk.css"
+  },
+  {
+    "revision": "ef6d552244850b3768a8",
+    "url": "/Prozak8/Zaks-Burgers/static/js/3.ef6d5522.chunk.js"
+  },
+  {
+    "revision": "eae50a9ca10711334a56",
+    "url": "/Prozak8/Zaks-Burgers/static/css/4.97f76789.chunk.css"
+  },
+  {
+    "revision": "eae50a9ca10711334a56",
+    "url": "/Prozak8/Zaks-Burgers/static/js/4.eae50a9c.chunk.js"
+  },
+  {
+    "revision": "caff5021911a6dd96187",
+    "url": "/Prozak8/Zaks-Burgers/static/css/5.8229955b.chunk.css"
+  },
+  {
+    "revision": "caff5021911a6dd96187",
+    "url": "/Prozak8/Zaks-Burgers/static/js/5.caff5021.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Prozak8/Zaks-Burgers/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "77e9ca30712a8b8f028cc64174e5a42c",
+    "url": "/Prozak8/Zaks-Burgers/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Prozak8/Zaks-Burgers/precache-manifest.9019aa68eff1ed04cab156e44e2392c3.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Prozak8/Zaks-Burgers/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
